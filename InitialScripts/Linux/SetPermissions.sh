d6d08d3f9453f868808a24fc13287b69c743a47a
acebird <acebird15@gmail.com>
2025-09-04 13:32:01 -0400

    Update SetPermissions.sh

--- InitialScripts/Linux/SetPermissions.sh
+++ InitialScripts/Linux/SetPermissions.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 #Set good permissions for common files
+
+echo "Changing permissions for /etc/shadow"
+sudo chown root /etc/shadow
+sudo chgrp root /etc/shadow
+sudo chmod 640 /etc/shadow
+
+echo "Changing permissions for /etc/group"
+sudo chown root /etc/group
+sudo chgrp root /etc/group
+sudo chmod 640 /etc/group
+
+# Change permissions for all config files
+
+
+
+
+#Change permissions for service specific files
