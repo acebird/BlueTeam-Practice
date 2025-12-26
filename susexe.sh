253b4633bbeffab5b88c1a5d4d60c507ec869ac8
Aidan Ethier <88387027+acebird@users.noreply.github.com>
2025-12-26 12:08:10 -0500

    Update susexe.sh

--- susexe.sh
+++ susexe.sh
@@ -3,11 +3,22 @@
 SEARCH_PATHS=(
     /home
     /tmp
-    /var/tmp
+    /var
     /opt
+    /mnt
 )
 
-find "${SEARCH_PATHS[@]}" \
-    -type f \
-    -perm /111 \
-    -printf '%p | %f | %TY-%Tm-%Td %TH:%TM:%TS\n' 2>/dev/null
+for path in "${SEARCH_PATHS[@]}"; do
+  if [[ "$path" == "/var" ]]; then
+    find /var \
+      -path /var/lib -prune -o \
+      -type f -perm /111 \
+      -printf '%p | %f | %TY-%Tm-%Td %TH:%TM:%TS\n' 2>/dev/null
+    
+  else
+    find "$path" \
+      -type f -perm /111 \
+      -printf '%p | %f | %TY-%Tm-%Td %TH:%TM:%TS\n' 2>/dev/null
+
+  fi
+done
